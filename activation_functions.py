V6 Modify Bug in sigmoid, Computer Derivatives in fit

activation_functions.py
@@ -25,7 +25,7 @@ class Sigmoid:
 
     @staticmethod
     def differentiate(z: np.ndarray):
-        d1 = np.exp(z)
+        d1 = np.exp(-z)
         d2 = np.multiply(1 + np.exp(-z), 1 + np.exp(-z))
         d = d1 / d2
         # d = np.exp(-z) / ((1 + np.exp(-z)) ** 2)

main.py
@@ -2,22 +2,27 @@ from layers import Dense
 import numpy as np
 import activation_functions as af
 import neural_network as nn
-from losses import BinaryCrossEntropy
+from losses import BinaryCrossEntropy, MeanSquaredError
 
-x = np.array([[0, 2]])
+x = np.array([[11]])
 y = np.array([1])
+
+layer1 = Dense(units=3, activation=af.Relu())
+# layer2 = Dense(units=2, activation=af.Relu())
+layer3 = Dense(units=1, activation=af.Sigmoid())
 
 model = nn.Sequential(
     [
-        Dense(units=100, activation=af.Sigmoid()),
-        Dense(units=10, activation=af.Sigmoid()),
-        Dense(units=1, activation=af.Sigmoid())
+        # layer1,
+        layer3
     ]
 )
 
-# print(model(x))
+print(model(x), "\n\n\n")
 
 model.compile(loss=BinaryCrossEntropy())
 model.fit(x, y)
+
+print("\n\n\n")
 
 # print(model.number_of_params())

neural_network.py
@@ -54,12 +54,25 @@ class Sequential:
         for example_num in range(training_examples):
             z_stack, a_stack = self._create_stacks_fwd_prop(x[example_num])
 
-            self.loss.differentiate(a_stack.top(), y[example_num])
+            dj_da = self.loss.differentiate(a_stack.top(), y[example_num])
 
             for layer in self.layers[::-1]:
-                da_dz = layer.activation.differentiate(z_stack.top())
+                cur_layer_activation = layer.activation
+                cur_layer_units = layer.get_num_of_units()
+                cur_layer_weights, _ = layer.get_weights()
+
+                da_dz = cur_layer_activation.differentiate(z_stack.top())
                 z_stack.pop()
-                print(da_dz, "\n")
+
+                a_stack.pop()
+                dz_dw = np.tile(a_stack.top(), (cur_layer_units, 1))
+
+                dj_db = np.multiply(dj_da.T, da_dz.T)
+                dj_dw = np.multiply(dj_db, dz_dw)
+                dj_dw = dj_dw.T
+                dj_da = dj_da @ (np.multiply(da_dz.T, cur_layer_weights.T))
+
+                print(dj_db, "\n")
 
     def compile(self, loss):
         self.loss = loss